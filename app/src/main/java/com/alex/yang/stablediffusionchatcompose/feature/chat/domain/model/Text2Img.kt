70cce2484bf12e42ca9a70767fef74804365514f
Alex <m9939418@gmail.com>
2025-12-05 23:31:23 +0800

    add Domain model

---
+++ app/src/main/java/com/alex/yang/stablediffusionchatcompose/feature/chat/domain/model/Text2Img.kt
@@ -0,0 +1,11 @@
+package com.alex.yang.stablediffusionchatcompose.feature.chat.domain.model
+
+/**
+ * Created by AlexYang on 2025/12/5.
+ *
+ *
+ */
+data class Text2Img(
+    val id: Long,
+    val imageUrl: String
+)
