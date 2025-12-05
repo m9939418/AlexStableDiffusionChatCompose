525b285c2829152795e96a193efefe5eea785cd0
Alex <m9939418@gmail.com>
2025-12-05 18:50:18 +0800

    init hilt

---
+++ app/src/main/java/com/alex/yang/stablediffusionchatcompose/App.kt
@@ -0,0 +1,12 @@
+package com.alex.yang.stablediffusionchatcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by AlexYang on 2025/12/5.
+ *
+ *
+ */
+@HiltAndroidApp
+class App : Application()
--- app/src/main/java/com/alex/yang/stablediffusionchatcompose/MainActivity.kt
+++ app/src/main/java/com/alex/yang/stablediffusionchatcompose/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.alex.yang.stablediffusionchatcompose.ui.theme.AlexStableDiffusionChatComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
